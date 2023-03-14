f3d343f772a29de3f39d0104287075b71802ec6c
feng <1589654294@qq.com>
2023-03-14 19:45:54 +0800

    save

---
+++ dreamland/app/src/main/java/com/example/dreamland/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.dreamland;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
