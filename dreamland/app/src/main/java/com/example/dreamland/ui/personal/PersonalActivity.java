095ef6f22c165bd90c284d14c5600bd27d87cf6f
雨 <2823316458@qq.com>
2023-03-30 14:30:26 +0800

    托梦和其他界面

--- dreamland/app/src/main/java/com/example/dreamland/ui/personal/PersonalActivity.java
+++ dreamland/app/src/main/java/com/example/dreamland/ui/personal/PersonalActivity.java
@@ -1,7 +1,10 @@
 package com.example.dreamland.ui.personal;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
@@ -14,49 +17,79 @@ import com.example.dreamland.databinding.ActivityPersonalBinding;
 import com.example.dreamland.entity.Dream;
 import com.example.dreamland.entity.User;
 import com.example.dreamland.ui.adapter.DreamAdapter;
+import com.example.dreamland.ui.chat.ChatActivity;
+import com.example.dreamland.ui.dashboard.DashboardActivity;
+import com.example.dreamland.ui.dreams.DreamsActivity;
+import com.example.dreamland.ui.layout.NavigationBar;
 import com.google.android.material.color.DynamicColors;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.tabs.TabLayout;
+import org.jetbrains.annotations.NotNull;
 
 import java.sql.Timestamp;
 import java.util.LinkedList;
 import java.util.List;
 
+import static androidx.core.content.ContextCompat.startActivity;
+
 public class PersonalActivity extends AppCompatActivity {
 
-        private DrawerLayout drawerLayout;
+    private DrawerLayout drawerLayout;
 
-        private DreamAdapter dreamAdapter;
+    private DreamAdapter dreamAdapter;
 
-        private ActivityPersonalBinding binding;
+    private ActivityPersonalBinding binding;
 
-        private List<Dream> dreams = new LinkedList<>();
+    private List<Dream> dreams = new LinkedList<>();
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            DynamicColors.applyToActivityIfAvailable(this);
-            super.onCreate(savedInstanceState);
-            // 绑定v层
-            binding = ActivityPersonalBinding.inflate(getLayoutInflater());
-            setContentView(binding.getRoot());
-            // 隐藏系统自带的标题栏
-            ActionBar actionBar = getSupportActionBar();
-            if (actionBar != null) {
-                actionBar.hide();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        DynamicColors.applyToActivityIfAvailable(this);
+        super.onCreate(savedInstanceState);
+        // 绑定v层
+        binding = ActivityPersonalBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        // 隐藏系统自带的标题栏
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+        // 设置侧边栏
+        binding.topAppBar.setNavigationIcon(R.drawable.menu);
+        // 设置navigation button 点击事件
+        binding.topAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        //设置上部菜单的监听事件
+        binding.tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                System.out.println("选中了他");
+
             }
 
-            // 设置侧边栏
-            binding.topAppBar.setNavigationIcon(R.drawable.menu);
-            // 设置navigation button 点击事件
-            binding.topAppBar.setNavigationOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
-                    drawerLayout.openDrawer(GravityCompat.START);
-                }
-            });
-            this.initList();
-        }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                System.out.println("没有选中他");
+            }
 
-        public void initList() {
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                System.out.println("重复选中了他");
+            }
+        });
+    }
+
+
+        public void initList () {
             User user = new User();
             user.setUsername("用户12345985");
 
@@ -77,5 +110,4 @@ public class PersonalActivity extends AppCompatActivity {
             recyclerView.setLayoutManager(layout);
             recyclerView.setAdapter(this.dreamAdapter);
         }
-
     }
