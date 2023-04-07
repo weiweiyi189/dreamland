24778aba9f6a7cdd0211f78bb4540c83b046c97b
雨 <2823316458@qq.com>
2023-04-07 18:22:05 +0800

    登陆、注册、个人信息、设置界面基本完成

--- dreamland/app/src/main/java/com/example/dreamland/ui/auth/LoginActivity.java
+++ dreamland/app/src/main/java/com/example/dreamland/ui/auth/LoginActivity.java
@@ -4,15 +4,16 @@ import android.content.Intent;
 import android.os.Build;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowInsets;
 import android.view.WindowInsetsController;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.EditText;
 import com.example.dreamland.R;
-import com.example.dreamland.databinding.ActivityLoginBinding;
 import com.example.dreamland.entity.User;
 import com.example.dreamland.service.BaseHttpService;
 import com.example.dreamland.service.UserService;
@@ -25,11 +26,26 @@ import com.google.android.material.snackbar.Snackbar;
  */
 public class LoginActivity extends AppCompatActivity {
 
+    private long exitTime = 0;
 
-    //android 捕获返回（后退）按钮事件
-    public void onBackPressed() {
-        overridePendingTransition(0,0);
-        finish();
+    //两次返回，返回到home界面（System.exit决定是否退出当前界面，重新加载程序）
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN) {
+
+            if ((System.currentTimeMillis() - exitTime) > 2000) {
+                Toast.makeText(getApplicationContext(), "再按一次退出程序", Toast.LENGTH_SHORT).show();
+                exitTime = System.currentTimeMillis();
+            } else {
+                Intent home = new Intent(Intent.ACTION_MAIN);
+                home.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                home.addCategory(Intent.CATEGORY_HOME);
+                startActivity(home);
+                //退出系统，不保存之前页面
+                System.exit(0);
+            }
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 
     private final UserService userService = UserService.getInstance();
@@ -53,7 +69,10 @@ public class LoginActivity extends AppCompatActivity {
         final EditText usernameEditText = findViewById(R.id.username);
         final EditText passwordEditText = findViewById(R.id.password);
         final Button loginButton = findViewById(R.id.login);
+        final TextView enroll = findViewById(R.id.tv_register);
+        final TextView forgotPassword = findViewById(R.id.tv_forgotPassword);
 
+        //登陆按键绑定事件
         loginButton.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
@@ -87,6 +106,24 @@ public class LoginActivity extends AppCompatActivity {
                     }
                 }
         );
+
+        //注册界面跳转
+        enroll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, EnrollActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //找回密码界面跳转
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, ForgetActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
--- dreamland/app/src/main/java/com/example/dreamland/ui/setting/SettingActivity.java
+++ dreamland/app/src/main/java/com/example/dreamland/ui/setting/SettingActivity.java
@@ -4,35 +4,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.dreamland.R;
+import com.example.dreamland.entity.User;
+import com.example.dreamland.service.BaseHttpService;
+import com.example.dreamland.service.UserService;
+import com.example.dreamland.ui.auth.EnrollActivity;
+import com.example.dreamland.ui.auth.LoginActivity;
 import com.example.dreamland.ui.dashboard.DashboardActivity;
 import com.example.dreamland.ui.dreams.DreamsActivity;
 import com.google.android.material.appbar.MaterialToolbar;
 
 public class SettingActivity extends AppCompatActivity {
 
-//    private long exitTime = 0;
-//    //两次返回，返回到home界面（System.exit决定是否退出当前界面，重新加载程序）
-//    public boolean onKeyDown(int keyCode, KeyEvent event) {
-//        if(keyCode == android.view.KeyEvent.KEYCODE_BACK && event.getAction() == android.view.KeyEvent.ACTION_DOWN){
-//
-//            if((System.currentTimeMillis()-exitTime) > 2000){
-//                Toast.makeText(getApplicationContext(), "再按一次退出程序", Toast.LENGTH_SHORT).show();
-//                exitTime = System.currentTimeMillis();
-//            } else {
-//                Intent home = new Intent(Intent.ACTION_MAIN);
-//                home.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//                home.addCategory(Intent.CATEGORY_HOME);
-//                startActivity(home);
-//                //退出系统，不保存之前页面
-//                System.exit(0);
-//            }
-//            return true;
-//        }
-//        return super.onKeyDown(keyCode, event);
-//    }
+    private final UserService userService = UserService.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,12 +28,89 @@ public class SettingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_setting);
 
         //导航栏返回按钮
-        MaterialToolbar topAppBar=findViewById(R.id.topAppBar);
+        MaterialToolbar topAppBar = findViewById(R.id.topAppBar);
         topAppBar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(SettingActivity.this, DashboardActivity.class);
                 startActivity(intent, null);
+            }
+        });
+
+        LinearLayout avatar = findViewById(R.id.avatar);
+        LinearLayout account = findViewById(R.id.account);
+        LinearLayout email = findViewById(R.id.email);
+        LinearLayout password = findViewById(R.id.password);
+        LinearLayout about = findViewById(R.id.about);
+        LinearLayout exit = findViewById(R.id.exit);
+
+        TextView accounts = findViewById(R.id.accounts);
+        TextView emails = findViewById(R.id.emails);
+
+
+
+        // 加载个人信息
+        userService.getCurrentUser(new BaseHttpService.CallBack() {
+            @Override
+            public void onSuccess(BaseHttpService.CustomerResponse result) {
+                //获取当前登陆用户
+                User currentUser= (User) result.getData();
+                System.out.println(currentUser.getUsername());
+                // 加载个人信息成功
+                if (result.getResponse().code() >= 200 && result.getResponse().code() < 300) {
+                    System.out.println(currentUser.getImageUrl());
+                    accounts.append("        "+currentUser.getUsername());
+                    emails.append("        "+currentUser.getUsername());
+                } else {
+                    // 登陆失败 提示错误
+                    Toast.makeText(SettingActivity.this, "信息预加载失败", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        //修改信息绑定
+        account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SettingActivity.this, AccountActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SettingActivity.this, EmailActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SettingActivity.this, PasswordActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        //关于我们
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SettingActivity.this, AboutActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        //退出登陆
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SettingActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
     }
